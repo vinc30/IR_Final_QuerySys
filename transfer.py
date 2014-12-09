6c9de0a3fd0499d916b91e4ec96162ca9a276ca1
吳柏瑜 <wupoyu@wuboyudeMacBook-Pro.local>
2014-12-09 14:06:27 +0800

    use beautifulsoup to parse xml

--- transfer.py
+++ transfer.py
@@ -4,22 +4,31 @@ import os
 import sys
 import split
 from glob import glob 
-import xml.etree.ElementTree as ET  
+from bs4 import BeautifulSoup
+#import xml.etree.ElementTree as ET  
 reload(sys)
 sys.setdefaultencoding('utf8')
 def load_xml_file(filename,test_word):
 #    print root.tag    
-#    workpath = os.getcwd()
     slist = []
-    root = ET.parse(filename).getroot()
-    temp = root[0][2]
-    for child in temp:
-        s1 = child.text
-#print s1
-        if s1 is None:
+#    tree =  ET()
+#   tree = ET.parse(filename)
+#    print root.tag
+#temp = root[0][1]
+#   print tree.tag
+#    count =0
+    string = open(filename).read()
+    soup = BeautifulSoup(string)
+    for i in soup.findAll('p'):
+        content = i.get_text()
+#       print s1
+        if content is None:
             pass
         else :
-            slist+=split.spli(s1)
+            slist+=split.spli(content)
+#       count +=1
+#       if count==3:
+#           break
 #        for i in range(0,len(slist)):
 #           if(test_word in slist[i]):
 #               print slist[i],"yaya"
@@ -27,19 +36,22 @@ def load_xml_file(filename,test_word):
 #               print slist[i]
 #    for i in range(0,len(slist)):
 #       print slist[i]
-    return slist
+    print filename
+"""
+    for i in range(0,len(slist)):
+        print slist[i]"""    
 #str_symptom = str(li).replace('u\'','\'')
 #       str_symptom.decode("utf8-escape")
 #        temp = [x.encode('utf-8') for x in li]
 if __name__ == '__main__':
-    f = open('query.txt')
+#    f = open('query.txt')
 #   for l in f:
 #       temp_test=l.strip().split(' ').pop()
 #       break
 #   for i in temp_test:
 #       temp_test.replace(" ","")
 # print temp_test
-    workpath = os.getcwd()
-#    DATA = '/Users/wupoyu/Desktop/IR/github/IR_Final_QuerySys/database/*'
-#   for f in glob(DATA):
-    load_xml_file("database/100093" ,u"億")
+    DATA = '/Users/wupoyu/Desktop/IR/github/IR_Final_QuerySys/database/*'
+    for f in glob(DATA):
+        load_xml_file(f ,u" ")
+#    load_xml_file("database/100180" ,u" ")
