9cf5b21b7baeef5ce691f3c6cdb73b89823afc55
吳柏瑜 <wupoyu@wuboyudeMacBook-Pro.local>
2014-12-10 14:54:13 +0800

    註解

--- test.py
+++ test.py
@@ -15,9 +15,8 @@ for i in glob(DATAPATH):
     for i in soup.findAll('p'):
         content += i.get_text()
     testlist.append(content)
-#print testlist
 
-#sentences = ["我喜欢吃土豆","土豆是个百搭的东西","我不喜欢今天雾霾的北京"]
+#testlist = ["我喜欢吃土豆","土豆是个百搭的东西","我不喜欢今天雾霾的北京"]
 words=[]
 for doc in testlist:
     words.append(list(testlist))
@@ -25,18 +24,18 @@ for doc in testlist:
 dic = corpora.Dictionary(words)
 #print dic
 #print dic.token2id
+
 #for word,index in dic.token2id.iteritems():
 #   print word +" 编号为:"+ str(index)
-corpus = [dic.doc2bow(text) for text in words]
-#print corpus
+corpus = [dic.doc2bow(text) for text in words] #print corpus
 
 tfidf = models.TfidfModel(corpus)
-vec = [(0, 1), (4, 1)]
-#print tfidf[vec]
+vec = [(0, 1), (4, 1)] #print tfidf[vec]
 corpus_tfidf = tfidf[corpus]
-#for doc in corpus_tfidf:
-#pass
-#print doc 
-index = similarities.SparseMatrixSimilarity(tfidf[corpus], num_features=1109)
+"""
+for doc in corpus_tfidf:
+    print doc 
+"""
+index = similarities.SparseMatrixSimilarity(tfidf[corpus], num_features=1109)#need to get from print dic 25
 sims = index[tfidf[vec]]
 print list(enumerate(sims))
