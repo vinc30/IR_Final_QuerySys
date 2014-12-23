be61164c0e32ed118a1b3682557580b56d65f064
吳柏瑜 <wupoyu@wuboyudeMacBook-Pro.local>
2014-12-23 16:55:48 +0800

    test

--- classify/test.py
+++ classify/test.py
@@ -1,45 +1,43 @@
+from bs4 import BeautifulSoup
 from textblob.classifiers import NaiveBayesClassifier
 from textblob import TextBlob
-#from text.blob import TextBlob
-#-m textblob.download_corpora
-#from nltk import NaiveBayesClassifier
-train = [
-         ('I love this sandwich.', '1'),
-         ('This is an amazing place!', '1'),
-         ('I feel very good about these beers.', '1'),
-         ('This is my best work.', '1'),
-         ("What an awesome view", '1'),
-         ('I do not like this restaurant', '2'),
-         ('I am tired of this stuff.', '2'),
-         ("I can't deal with this", '2'),
-         ('He is my sworn enemy!', '2'),
-         ('My boss is horrible.', '2'),
-         ('I like the girl','0'),
-         ('This place is huge','0'),
-         ('Mac is better than windows','0')
-         ]
-test = [
-        ('The beer was good.', 'pos'),
-        ('I do not enjoy my job', 'neg'),
-        ("I ain't feeling dandy today.", 'neg'),
-        ("I feel amazing!", 'pos'),
-        ('Gary is a friend of mine.', 'pos'),
-        ("I can't believe I'm doing this.", 'neg')
-        ]
+train = []
+f = open('news_to_news_random_id_unlabeled_table_groundtruth_no_original_id.txt','r')  #need to change file path
+for line in f:
+    content =""
+    line = line.split()
+    date = line.pop()
+    news = line.pop()
+    num_id = line.pop()
+    print num_id
+    fp = open('../github/IR_Final_QuerySys/database/news_random_id_unlabeled_new/'+ num_id,'r') #need to chnage file path
+    soup = BeautifulSoup(fp)
+    content+=soup.find('title').get_text()
+    for i in soup.findAll('p'):
+        content += i.get_text()
+    train.append( (content,int(news)) )
+    fp.close()
+#print w
 cl = NaiveBayesClassifier(train)
-print(cl.classify("Their burgers are amazing."))  # "pos"
-print(cl.classify("I don't like their pizza."))   # "neg"
-blob = TextBlob("The beer was amazing. But the hangover was horrible. "
-                "My boss was not pleased.", classifier=cl)
-print(blob)
-print(blob.classify())
-
-for sentence in blob.sentences:
-    print(sentence)
-    print(sentence.classify())
-
-# Compute accuracy
-print("Accuracy: {0}".format(cl.accuracy(test)))
-
-# Show 5 most informative features
-cl.show_informative_features(5)
+print ('done')
+content = ""
+fp = open('../github/IR_Final_QuerySys/database/news_random_id_unlabeled_new/67065','r') # need to change file path
+soup = BeautifulSoup(fp)
+content+=soup.find('title').get_text()
+for i in soup.findAll('p'):
+    content += i.get_text()
+print (cl.classify(content))
+content = ""
+fp = open('../github/IR_Final_QuerySys/database/news_random_id_unlabeled_new/47238','r') # need to change file path
+soup = BeautifulSoup(fp)
+content+=soup.find('title').get_text()
+for i in soup.findAll('p'):
+    content += i.get_text()
+print (cl.classify(content))
+content = ""
+fp = open('../github/IR_Final_QuerySys/database/news_random_id_unlabeled_new/29169','r')  # need to change file path
+soup = BeautifulSoup(fp)
+content+=soup.find('title').get_text()
+for i in soup.findAll('p'):
+    content += i.get_text()
+print (cl.classify(content))
